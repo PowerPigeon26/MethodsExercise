fd807887400e404272285378bfc86bdee0375790
Maverick Grayer <maverick72032@gmail.com>
2024-03-22 15:50:20 -0400

    cleaned code

--- MethodsExercise/obj/Debug/net6.0/MethodsExercise.AssemblyInfo.cs
+++ MethodsExercise/obj/Debug/net6.0/MethodsExercise.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MethodsExercise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31d5835be49aeeb4ed40d35702db3ebe696c8c2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MethodsExercise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MethodsExercise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
